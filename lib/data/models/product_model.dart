008a3bcefdcdcb8367aef8758dfe0c3f40b62a3e
Med Elmostapha <dedijekani@gmail.com>
2026-04-17 19:30:16 +0000

    feat(models): add product and category data models

---
+++ lib/data/models/product_model.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final String categoryId;
+  final int stock;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.categoryId,
+    required this.stock,
+  });
+}
