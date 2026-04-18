25e9a8b315385eef39d122d8b625533952436248
Med Elmostapha <dedijekani@gmail.com>
2026-04-18 20:13:46 +0000

    feat(dummy): update products model with minStock and createdAt support

--- lib/data/models/product_model.dart
+++ lib/data/models/product_model.dart
@@ -2,14 +2,20 @@ class Product {
   final String id;
   final String name;
   final double price;
-  final String categoryId;
   final int stock;
+
+  final int minStock;
+  final String categoryId;
+
+  final DateTime createdAt;
 
   Product({
     required this.id,
     required this.name,
     required this.price,
-    required this.categoryId,
     required this.stock,
+    required this.minStock,
+    required this.categoryId,
+    required this.createdAt,
   });
 }
