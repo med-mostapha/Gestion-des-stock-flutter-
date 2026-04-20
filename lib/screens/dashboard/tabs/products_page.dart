685e08f620687a6805396ff6e763affc43d07a0d
Med Elmostapha <dedijekani@gmail.com>
2026-04-20 18:52:50 +0000

    feat(products): add product list with search and navigation to add product page

--- lib/screens/dashboard/tabs/products_page.dart
+++ lib/screens/dashboard/tabs/products_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_de_stock_flutter/data/models/product_model.dart';
 import 'package:gestion_de_stock_flutter/data/repositories/product_repository.dart';
+import 'package:gestion_de_stock_flutter/screens/dashboard/tabs/add_product_page.dart';
 import 'package:gestion_de_stock_flutter/widgets/products/product_card.dart';
 import 'package:gestion_de_stock_flutter/widgets/ui/app_search_bar.dart';
 
@@ -34,22 +35,41 @@ class _ProductsPageState extends State<ProductsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SearchBarApp(onChanged: search),
-        Expanded(
-          child: ListView.separated(
-            padding: const EdgeInsets.all(12),
-            itemCount: filtered.length,
-            itemBuilder: (context, index) {
-              return ProductCard(product: filtered[index]);
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(height: 12);
-            },
+    return Scaffold(
+      body: Column(
+        children: [
+          SearchBarApp(onChanged: search),
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.all(12),
+              itemCount: filtered.length,
+              itemBuilder: (context, index) {
+                return ProductCard(product: filtered[index]);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 12);
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newProduct = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddProductPage()),
+          );
+
+          if (newProduct != null && newProduct is Product) {
+            setState(() {
+              products.add(newProduct);
+              filtered = products;
+            });
+          }
+        },
+        backgroundColor: Colors.white,
+        child: Icon(Icons.add, color: Colors.black),
+      ),
     );
   }
 }
