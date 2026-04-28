18e72f3aee5ede7cafbbcdb6235c886c73b8ec33
Med Elmostapha <dedijekani@gmail.com>
2026-04-28 00:02:49 +0000

    refactor: IndexPage uses Provider and AnalyticsService, add charts with skeleton loading

--- lib/screens/dashboard/tabs/index_page.dart
+++ lib/screens/dashboard/tabs/index_page.dart
@@ -6,46 +6,113 @@ import 'package:gestion_de_stock_flutter/providers/product_provider.dart';
 import 'package:gestion_de_stock_flutter/providers/category_provider.dart';
 import 'package:gestion_de_stock_flutter/widgets/dashboard/stats_card.dart';
 
+import '../../../data/ services/analytics_service.dart';
+import '../../../widgets/charts/category_percentage_pie_chart.dart';
+import '../../../widgets/charts/category_stock_bar_chart.dart';
+
 class IndexPage extends StatelessWidget {
   const IndexPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final products = context.watch<ProductProvider>().products;
-    final categories = context.watch<CategoryProvider>().categories;
+    final productProvider = context.watch<ProductProvider>();
+    final categoryProvider = context.watch<CategoryProvider>();
 
-    return GridView.count(
-      crossAxisCount: 2,
-      childAspectRatio: 2,
+    final isLoading = productProvider.isLoading || categoryProvider.isLoading;
 
+    final totalStockValue = AnalyticsService.getTotalStockValue(
+      productProvider.products,
+    );
+
+    final lowStock = AnalyticsService.getLowStockProducts(
+      productProvider.products,
+    ).length;
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(8),
+      child:
+          // isLoading
+          //     ? _buildChartsSkeleton()
+          //     :
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 2,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  StatsCard(
+                    title: "Products",
+                    subtitle: "",
+                    value: productProvider.totalProducts + .0,
+                    icon: Icons.inventory,
+                    iconbg: AppColors.primary,
+                  ),
+                  StatsCard(
+                    title: "Categories",
+                    subtitle: "",
+                    value: categoryProvider.totalCategories + .0,
+                    icon: Icons.category,
+                    iconbg: CupertinoColors.activeOrange,
+                  ),
+                  StatsCard(
+                    title: "Stock Value",
+                    subtitle: "MRU",
+                    value: totalStockValue,
+                    icon: Icons.account_balance_wallet,
+                    iconbg: AppColors.success,
+                  ),
+                  StatsCard(
+                    title: "Low Stock",
+                    subtitle: "alert",
+                    value: lowStock + .0,
+                    icon: Icons.warning_amber_rounded,
+                    iconbg: CupertinoColors.systemRed,
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              //bar chart
+              CategoryStockBarChart(
+                products: productProvider.products,
+                categories: categoryProvider.categories,
+              ),
+
+              const SizedBox(height: 16),
+
+              CategoryPercentagePieChart(
+                products: productProvider.products,
+                categories: categoryProvider.categories,
+              ),
+            ],
+          ),
+    );
+  }
+
+  Widget _buildChartsSkeleton() {
+    return Column(
       children: [
-        StatsCard(
-          title: "Products",
-          subtitle: "",
-          value: products.length + .0,
-          icon: Icons.inventory,
-          iconbg: AppColors.primary,
+        Container(
+          width: double.infinity,
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(16),
+          ),
         ),
-        StatsCard(
-          title: "Category",
-          subtitle: "",
-          value: categories.length + .0,
-          icon: Icons.category,
-          iconbg: CupertinoColors.activeOrange,
-        ),
-        const StatsCard(
-          title: "Money",
-          subtitle: "",
-          value: 12059,
-          icon: Icons.money_off_csred,
-          iconbg: CupertinoColors.systemPurple,
-        ),
-        const StatsCard(
-          title: "Wallet",
-          subtitle: "",
-          value: 4568,
-          icon: Icons.wallet,
-          iconbg: CupertinoColors.activeGreen,
+        const SizedBox(height: 16),
+        Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(16),
+          ),
         ),
       ],
     );
